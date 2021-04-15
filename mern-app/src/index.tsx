92d59aa0b08a67a70eb58f1a1eb38d4415087acb
Samba <samba.pendela@gmail.com>
2021-04-15 08:02:05 +0530

    code

--- mern-app/src/index.tsx
+++ mern-app/src/index.tsx
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Products from "./Products";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Products />
   </React.StrictMode>,
   document.getElementById('root')
 );
